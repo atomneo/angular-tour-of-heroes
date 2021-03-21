2f4a9e1933f5eab9e46217cce3cc6ee31ff69e9d
Mateusz Leszner <mateusz@leszner.eu>
2021-03-21 14:37:50 +0100

    Part 2 - Display a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Anakin'},
+    {id: 12, name: 'Obi-Wan'},
+    {id: 13, name: 'Darth Maul'},
+    {id: 14, name: 'Darth Vader'},
+    {id: 15, name: 'Count Dooku'},
+    {id: 16, name: 'Qui Gon'},
+    {id: 17, name: 'Han Solo'},
+    {id: 18, name: 'Leia'},
+    {id: 19, name: 'Wedge'},
+    {id: 20, name: 'R2-D2'}
+];
